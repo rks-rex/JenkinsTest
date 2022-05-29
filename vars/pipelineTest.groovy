a48b9c617ed67220095be34c1fe4586646a6e934
rks-rex <roland.utz@protonmail.com>
2022-05-29 20:17:37 +0200

    changed structure #6

--- vars/pipelineTest.groovy
+++ vars/pipelineTest.groovy
@@ -1,7 +1,6 @@
-import helperCollection.helperFunctions
 
 
-def call(helperFunctions helpers) {
+def call() {
 
     pipeline {
         agent {
@@ -11,44 +10,46 @@ def call(helperFunctions helpers) {
             timeout(time: 90, unit: 'MINUTES')
             disableConcurrentBuilds()
         }
-        environment {
-            MLFW_BRANCH_NAME = makeMLFWBranchName()
-            MLFW_ENV = branchToMLFWEnv(MLFW_BRANCH_NAME)
 
-            DOCKER_REGISTRY_HOST = 'tasktrack.telekom.at'
-            DOCKER_TARGET_REPO = branchToDockerRepo(MLFW_BRANCH_NAME)
-            DOCKER_BRANCH_TAG = makeDockerTagName(MLFW_BRANCH_NAME)
 
-            JENKINS_DEPLOY_TOKEN = mlfwEnvToOsJenkinsDeployToken(MLFW_ENV)
-            CREDENTIAL_ANEN_ID = mlfwEnvToAnenCredentialsId(MLFW_ENV)
-            OS_SERVER = mlfwEnvToOSClusterURI(MLFW_ENV)
-            OS_PROJECT = mlfwEnvToOSProject(MLFW_ENV)
-            MODEL_NAME = gitRepositoryToModelName()
-            SONAR_PROJECT_KEY = gitRepositoryToSonarProjectKey()
-            FULL_TEST_POD_NAME = makePodName(MLFW_BRANCH_NAME, MODEL_NAME, 'fulltest')
-            UNIT_TEST_POD_NAME = makePodName(MLFW_BRANCH_NAME, MODEL_NAME, 'unittest')
-
-            def props = readProperties file: 'config.ini'
-            COVERAGE_FILE = return_identic(props.COVERAGE_FILE)
-            UNIT_EXECUTION_FILE_JENKINS = return_identic(props.UNIT_EXECUTION_FILE_JENKINS)
-
-            /** Dev Resources - in Prod Resources are defined via Airflow **/
-            CPU = "${cpuUnits}"
-            RAM = "${ramGb}"
-
-            /** Skip stages in Pipeline **/
-            SKIP_UNIT_TESTS = "${skipUnitTests}"
-            SKIP_FULL_TEST = "${skipFullTests}"
-
-            /** run arguments for full test **/
-            SKIP_UNIT_TESTS = "${skipUnitTests}"
-            SKIP_FULL_TEST = "${skipFullTests}"
-            arg1 = "${arg1}"
-            arg2 = "${arg2}"
-            arg3 = "${arg3}"
-            arg4 = "${arg4}"
-            arg5 = "${arg5}"
-        }
+//        environment {
+//            MLFW_BRANCH_NAME = makeMLFWBranchName()
+//            MLFW_ENV = branchToMLFWEnv(MLFW_BRANCH_NAME)
+//
+//            DOCKER_REGISTRY_HOST = 'tasktrack.telekom.at'
+//            DOCKER_TARGET_REPO = branchToDockerRepo(MLFW_BRANCH_NAME)
+//            DOCKER_BRANCH_TAG = makeDockerTagName(MLFW_BRANCH_NAME)
+//
+//            JENKINS_DEPLOY_TOKEN = mlfwEnvToOsJenkinsDeployToken(MLFW_ENV)
+//            CREDENTIAL_ANEN_ID = mlfwEnvToAnenCredentialsId(MLFW_ENV)
+//            OS_SERVER = mlfwEnvToOSClusterURI(MLFW_ENV)
+//            OS_PROJECT = mlfwEnvToOSProject(MLFW_ENV)
+//            MODEL_NAME = gitRepositoryToModelName()
+//            SONAR_PROJECT_KEY = gitRepositoryToSonarProjectKey()
+//            FULL_TEST_POD_NAME = makePodName(MLFW_BRANCH_NAME, MODEL_NAME, 'fulltest')
+//            UNIT_TEST_POD_NAME = makePodName(MLFW_BRANCH_NAME, MODEL_NAME, 'unittest')
+//
+//            def props = readProperties file: 'config.ini'
+//            COVERAGE_FILE = return_identic(props.COVERAGE_FILE)
+//            UNIT_EXECUTION_FILE_JENKINS = return_identic(props.UNIT_EXECUTION_FILE_JENKINS)
+//
+//            /** Dev Resources - in Prod Resources are defined via Airflow **/
+//            CPU = "${cpuUnits}"
+//            RAM = "${ramGb}"
+//
+//            /** Skip stages in Pipeline **/
+//            SKIP_UNIT_TESTS = "${skipUnitTests}"
+//            SKIP_FULL_TEST = "${skipFullTests}"
+//
+//            /** run arguments for full test **/
+//            SKIP_UNIT_TESTS = "${skipUnitTests}"
+//            SKIP_FULL_TEST = "${skipFullTests}"
+//            arg1 = "${arg1}"
+//            arg2 = "${arg2}"
+//            arg3 = "${arg3}"
+//            arg4 = "${arg4}"
+//            arg5 = "${arg5}"
+//        }
 
         stages {
             stage('Print Jobinfo') {
@@ -66,7 +67,8 @@ def call(helperFunctions helpers) {
 //          print "Name of the fulltest POD : ${env.FULL_TEST_POD_NAME}"
 //          print "Name of the unittest POD : ${env.UNIT_TEST_POD_NAME}"
 //          print '========================================================'
-                    helpers.printEnvVars()
+                    library('pipeline_helper').helperCollection.printEnvVars()
+
                 }
             }
         }
